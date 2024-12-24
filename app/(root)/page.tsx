1dd0b7b43ff741ddc3d52ba3a6cdd5bfac456f8b
Ethernal Knight <kaziarman@proton.me>
2024-12-24 16:50:25 +0600

    author authintication complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,23 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 const Home = async ({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) => {
-
   const query = (await searchParams).query;
+  const params = { search: query || null };
+
+  const session = await auth();
+
+  console.log(session?.id);
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
-  
+
   return (
     <>
       <section className="pink_container">
@@ -31,7 +38,7 @@ const Home = async ({
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
 
-        <ul className="mt-7 card_grid">
+        <ul className="card_grid mt-7">
           {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
@@ -41,6 +48,8 @@ const Home = async ({
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 };
